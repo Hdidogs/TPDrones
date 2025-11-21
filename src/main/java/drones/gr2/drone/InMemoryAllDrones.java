89c2d397e8423d5aa30865585f35956f7bc89a71
djibr <d.korchi@lprs.fr>
2025-11-21 11:32:50 +0100

    Exercice 3 de 3.1 à 3.5

--- src/main/java/drones/gr2/drone/InMemoryAllDrones.java
+++ src/main/java/drones/gr2/drone/InMemoryAllDrones.java
@@ -1,4 +1,46 @@
 package drones.gr2.drone;
 
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class InMemoryAllDrones implements AllDrones {
+    private ArrayList<Drone> drones = new ArrayList<>();
+    @Override
+    public void remove(String name) {
+        int index = -1;
+        for (int i = 0; i < drones.size(); i++) {
+            if (Objects.equals(drones.get(i).getNom(), name)){
+                index=i;
+            }
+        }
+        this.drones.add(drones.get(index));
+    }
+
+    @Override
+    public void add(Drone drone) {
+        this.drones.add(drone);
+    }
+
+    @Override
+    public void all() {
+        for (int i = 0; i < drones.size(); i++) {
+            System.out.println("name :"+ drones.get(i).getNom()+
+                    " Vitesse :"+drones.get(i).getVitesse()+
+                    "Position :"+ drones.get(i).getPosition().getX()+", "+
+                    drones.get(i).getPosition().getY()+", "+
+                    drones.get(i).getPosition().getZ());
+        }
+    }
+
+    @Override
+    public void findBy(String name) {
+        int index = -1;
+        for (int i = 0; i < drones.size(); i++) {
+            if(drones.get(i).getNom()==name){
+                index=i;
+            }
+        }
+
+    }
+
 }
