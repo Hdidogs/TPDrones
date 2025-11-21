f9b20e769694c8c9385b928322a7a48c32520012
djibr <d.korchi@lprs.fr>
2025-11-21 12:18:13 +0100

    correction de 3.5 et 3.4

--- src/main/java/drones/gr2/drone/AllDrones.java
+++ src/main/java/drones/gr2/drone/AllDrones.java
@@ -1,11 +1,15 @@
 package drones.gr2.drone;
 
+import drones.gr2.util.ActionResult;
+
+import java.util.ArrayList;
+
 public interface AllDrones {
     void remove(String name);
 
     void add(Drone drone);
 
-    void all();
+    ArrayList<Drone> all();
 
-    void findBy(String name);
+    ActionResult findBy(String name);
 }
